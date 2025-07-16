f7b96b2f97cfd2d140b3e6903735f2223f655109
Alexey <amlion2004@gmail.com>
2025-07-16 20:15:17 +0300

    not finished, but i yet go backward

---
+++ src/02_PushPip/cow_say.py
@@ -0,0 +1,58 @@
+import argparse
+import cowsay
+
+# in the future can be to add following options
+# --verbose -v
+# --help -h
+# --version
+
+# todo: rewrite help from https://pypi.org/project/python-cowsay/
+# todo: not processed cowfile argument in source code
+
+# docs:
+# 1 https://linux.die.net/man/1/cowsay
+# 2 https://pypi.org/project/python-cowsay/
+
+parser = argparse.ArgumentParser()
+parser.add_argument("message", type=str, action='store', help='the message to be displayed')
+parser.add_argument("--cow", type=str, action='store', help='use -f to know available cows can be found by calling list_cows', default='default')
+parser.add_argument("-l", "--list_cows", action='store_true', help='available cows can be found by calling list_cows')
+parser.add_argument('-e', '--eyes', '--eye_string', help='-e or --eye_string')
+parser.add_argument('-T', '--tongue', '--tongue_string', help='-T or --tongue_string')
+parser.add_argument('-W', '--width', '--column', help='-W or --width', type=int)
+parser.add_argument('--wrap_text', action='store', type=bool, help='--wrap_text')
+parser.add_argument('-n', action='store_true')
+parser.add_argument('-f', '--cowfile', help='a string containing the cow file text (chars are not'
+    ' decoded as they are in read_dot_cow) if this parameter is provided the'
+    ' cow parameter is ignored',default=None,type=str)
+parser.add_argument('--random', action='store_true', help='If provided, picks a random cow from the COWPATH.'
+        '\nIs superseded by the -f option')
+# parser.add_argument('-h', '--help', action='store_true', help='execute print_help method')
+parser.add_argument('--usage', action='store_true', help='execute print_usage method')
+
+args = parser.parse_args()
+print(args)
+#
+# if args.list_cows:
+#     print('list of available cows is: ', cowsay.list_cows())
+
+
+# if args.help:
+#     parser.print_help()
+if args.usage:
+    parser.print_usage()
+
+
+# exist debuging options: --list_of_cows and another
+# cowsay.cowsay(args.message, )
+# {action: getattr(action, 'dest') for action in parser._actions if action.option_strings}
+# [param_name for param_name in args.__dict__ if ]
+
+# cowsay.cowsay()
+
+# if args.random:
+#     cow = cowsay.get_random_cow()
+#     if args.cow !=
+# todo: finish it
+
+parser.exit()
