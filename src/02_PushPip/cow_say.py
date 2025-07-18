f5283ed0b732ecd2e949b2d80a5e015ceae9db70
Alexey <amlion2004@gmail.com>
2025-07-19 01:28:31 +0300

    completed write command line option for cowsay. in waiting write base logic

--- src/02_PushPip/cow_say.py
+++ src/02_PushPip/cow_say.py
@@ -3,19 +3,29 @@ import cowsay
 
 # in the future can be to add following options
 # --verbose -v
-# --help -h
+# --help -h - default exist
 # --version
 
 # todo: rewrite help from https://pypi.org/project/python-cowsay/
 # todo: not processed cowfile argument in source code
 
+# reminder
+# distinguish const, default parameters in add_argument !!!
+
 # docs:
 # 1 https://linux.die.net/man/1/cowsay
 # 2 https://pypi.org/project/python-cowsay/
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="cowsay console wrapper used argparse packet",
+                                 prog="cowsay argparse script",
+                                 usage="hard to describe why it need to you",
+                                 add_help=True, # --help, -h
+                                 epilog="output in console text before help by any accepted argument",
+                                 )
+
 parser.add_argument("message", type=str, action='store', help='the message to be displayed')
-parser.add_argument("--cow", type=str, action='store', help='use -f to know available cows can be found by calling list_cows', default='default')
+parser.add_argument("--cow", type=str, action='store', help='use -f to know available cows can be found by calling list_cows', default='default',
+                    choices=cowsay.list_cows(), nargs='?') # if specify the const par, then --cow without value seem strange
 parser.add_argument("-l", "--list_cows", action='store_true', help='available cows can be found by calling list_cows')
 parser.add_argument('-e', '--eyes', '--eye_string', help='-e or --eye_string')
 parser.add_argument('-T', '--tongue', '--tongue_string', help='-T or --tongue_string')
@@ -29,6 +39,9 @@ parser.add_argument('--random', action='store_true', help='If provided, picks a 
         '\nIs superseded by the -f option')
 # parser.add_argument('-h', '--help', action='store_true', help='execute print_help method')
 parser.add_argument('--usage', action='store_true', help='execute print_usage method')
+
+for cow_customization in cowsay.COW_OPTIONS:
+    parser.add_argument('-{}'.format(cow_customization), action='store_true', help='customize tongue and eyes from the box')
 
 args = parser.parse_args()
 print(args)
